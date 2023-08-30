2889572fec2e0d03a39ec458fff1f26f9a0ff4dc
D-Schecht <91809730+D-Schecht@users.noreply.github.com>
2023-08-30 19:52:41 -0400

    Next version of eliza code

--- eliza.py
+++ eliza.py
@@ -16,18 +16,23 @@
 # --------- Import Libraries
 import re
 import nltk
+#nltk.download('words')
 from nltk.tokenize import word_tokenize, RegexpTokenizer
 from nltk.corpus import stopwords
-from nltk import pos_tag
+from nltk import pos_tag, ne_chunk
 from nltk.probability import FreqDist
 from nltk.corpus import words 
+from nltk.tree import Tree
 import random 
 
 
 # --------- Helpful Lists & Dictionaries
 
+# List of Stop Words
+stop_words = stopwords.words('english')
+
 # pre-made list of user-input ending statements
-ending_statements = ['bye','goodbye','good bye','thank you', 'see you next time', 'go away eliza', 'bye eliza','good bye eliza',' goodbye eliza']
+ending_statements = ['end','bye','goodbye','good bye','thank you', 'see you next time', 'go away eliza', 'bye eliza','good bye eliza',' goodbye eliza']
 
 # Pre-made responses for when Eliza is confused
 confused_responses = ["Why don't you elaborate on that to help me understand?",
@@ -82,16 +87,127 @@ def explode_contractions(input_phrase):
 
 
 # Retreive Names
+def get_names(no_stop_words_text, userinput):
+    name_text = []
+    
+    namesearch = re.search(r'.[A-Z][a-z]*',userinput)
 
+    
+    for word in no_stop_words_text:
+        print(f"word is {word}")
+        if word[1] !='NNP' and word[0]==str(namesearch.group()).strip():
+            name_text.append((word[0],'NNP'))
+        else:
+            name_text.append(word)
+
+    
+
+    name = [word[0] for word in name_text if word[1] in ['NNS','NNP','NN'] and re.match(r'[A-Z][a-z]*',word[0])]
+    
+    print(f"name is {name}")
+
+
+    return name, name_text
+
+# remove nonsense words
+def remove_nonsense(userinput):
+    nonsense = []
+    nonsense = [word for word in userinput]
+
+    sensical_words = []
+    for item in nonsense:
+        if item[0].lower() in words.words():
+            sensical_words.append(item)
+    
+    return sensical_words
+
+# Generate a Response
+def get_response(userinput,postype):
+
+    
+
+    return response
 
 #TODO: This is where the actual logic flow will go
 # Pre-processing & Response Generation
 def preprocess(userinput):
 
-    confused = True # using True for now to test
+    confused = False # using True for now to test
 
-    if confused:
-        response = eliza_confused(confused)
+    response = "Tell me about something else?"
+    
+    this_chunk, nounphrases = [], []
+
+    # Explode any contractions
+    userinput_exploded = explode_contractions(userinput)
+
+    # Tokenize Words
+    user_words = word_tokenize(userinput_exploded)
+
+    # Tag part of speech
+    pos_user_words = pos_tag(user_words)
+    
+
+    # find noun phrases
+    ne_tree = ne_chunk(pos_user_words)
+    grammar = 'NP: {<DT>?<JJ>*<NN>}'
+    chunkparser = nltk.RegexpParser(grammar)
+
+    user_noun_phrase = chunkparser.parse(pos_user_words)
+
+    # Retreive noun phrases
+    for chunk in user_noun_phrase:
+        if type(chunk) == Tree:
+            this_chunk = f' {[token for token, partofspeech in chunk.leaves()]}'
+            nounphrases.append(this_chunk)
+
+    # remove stop words
+    
+    cleaned_words = [word for word in pos_user_words if word[0].lower() not in stop_words]
+    
+
+    # Perform Word Spotting
+    name, adjective, verb,adverb, noun = '','','','',''
+
+    if re.search(r'.[A-Z][a-z]*',userinput):
+        print('name found')        
+        # This will handle the case where end users enter a name 
+        # TODO: Bug - "My name is Probably Devin" returns name as "Probably"
+        name, non_name_text = get_names(cleaned_words, userinput)
+        
+        # This will handle verbs
+        verb = [word[0] for word in non_name_text if word[1] in ['VBG','VBZ','VBD','VBN','VBP','VB']]
+
+        # This will handle adverbs
+        adverb = [word[0] for word in non_name_text if word[1] in['RBR','RBS','RB','RP']]
+
+        # This will handle adjectives
+        adjective = [word[0] for word in non_name_text if word[1] in ['JJ','JJR','JJS']]
+
+    else:
+        print('name not found')
+        # This will handle nouns
+        noun = [word[0] for word in cleaned_words if word[1] in ['NNS','NN']]
+
+        # This will handle verbs
+        verb = [word[0] for word in cleaned_words if word[1] in ['VBG','VBZ','VBD','VBN','VBP','VB']]
+
+        # This will handle adverbs
+        adverb = [word[0] for word in cleaned_words if word[1] in['RBR','RBS','RB','RP']]
+
+        # This will handle adjectives
+        adjective = [word[0] for word in cleaned_words if word[1] in ['JJ','JJR','JJS']]
+
+    
+    if name:
+        
+        response=[f"Hello {name[0]}! It's great to see you. How are you?"]
+    
+    if adjective:
+        response = get_response(userinput,adjective[0])
+
+    else:
+        response = eliza_confused(confused=True)
 
 
 
@@ -103,9 +219,14 @@ def preprocess(userinput):
 # Run Eliza
 def Eliza():
 
+    start = True
+
     while True:
         # start off by asking introducting Eliza and asking for your name.
-        print("Eliza: Hello - I'm Eliza! Can you tell me your name?")
+        if start:
+            print("Eliza: Hello - I'm Eliza! Can you tell me your name?")
+            start = False
+
         userinput = input("You: ")
 
         # Check if the user is ending the conversation
